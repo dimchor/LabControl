19b4d0970bc52b74594a70a82b9b7b5d41b89803
dimmed-light <113694089+dimmed-light@users.noreply.github.com>
2025-05-01 21:03:05 +0300

    added 10 sec delay

--- python/utilities.py
+++ python/utilities.py
@@ -4,9 +4,9 @@ from os import system
 def shutdown():
     ret = 0
     if platform == "linux":
-        ret = system("poweroff")
+        ret = system("sleep 10; poweroff")
     elif platform == "win32":
-        ret = system("shutdown /s /t 0")
+        ret = system("shutdown /s /t 10")
     else
         raise Exception(f"unable to poweroff: unsupported platform {platform}")
     if ret:
@@ -15,9 +15,9 @@ def shutdown():
 def reboot():
     ret = 0
     if platform == "linux":
-        ret = system("reboot")
+        ret = system("sleep 10; reboot")
     elif platform == "win32":
-        ret = system("shutdown /r /t 0")
+        ret = system("shutdown /r /t 10")
     else
         raise Exception(f"unable to reboot: unsupported platform {platform}")
     if ret:
